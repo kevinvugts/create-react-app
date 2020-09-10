1d424c89d39e15eb51070abf73fb6fcf6a524071
Kevin Vugts <k.vugts@student.fontys.nl>
2020-09-10 17:41:46 +0200

    Added missing aliases

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -719,6 +719,15 @@ module.exports = function (webpackEnv) {
           // The formatter is invoked directly in WebpackDevServerUtils during development
           formatter: isEnvProduction ? typescriptFormatter : undefined,
         }),
+
+      // Customizations Enorm
+      new webpack.DefinePlugin({
+        APP_ENVIRONMENT: 'development',
+        APP_CONFIG: JSON.stringify({
+          apiHost: process.env.API_HOST,
+          sendMailEndPoint: process.env.SEND_MAIL_ENDPOINT,
+        }),
+      }),
     ].filter(Boolean),
     // Some libraries import Node modules but don't use them in the browser.
     // Tell webpack to provide empty mocks for them so importing them works.
